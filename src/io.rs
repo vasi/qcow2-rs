b271c2ce51c6421931c303b7e585547b8863d5b3
Dave Vasilevsky <dave@vasilevsky.ca>
2016-07-05 10:30:44 -0400

    wip

--- src/io.rs
+++ src/io.rs
@@ -1,38 +1,62 @@
 extern crate byteorder;
-use self::byteorder::{ByteOrder};
+use self::byteorder::{ByteOrder, ReadBytesExt};
 
-use std;
 use std::marker::PhantomData;
+use std::io::{Result, Read};
 use super::pread::Pread;
 
 // Allow simple reading of integer types.
 pub trait ReadInt {
-    fn read_u32(&mut self) -> std::io::Result<u32>;
-    fn read_u64(&mut self) -> std::io::Result<u64>;
+    fn read_uint(&mut self, nbytes: usize) -> Result<u64>;
+    fn read_u32(&mut self) -> Result<u32> {
+        self.read_uint(4).map(|u| u as u32)
+    }
+    fn read_u64(&mut self) -> Result<u64> {
+        self.read_uint(8)
+    }
 }
 pub trait PreadInt {
-    fn pread_uint(&mut self, nbytes: usize, pos: u64) -> std::io::Result<u64>;
-    fn pread_u64(&mut self, pos: u64) -> std::io::Result<u64> {
+    fn pread_uint(&mut self, nbytes: usize, pos: u64) -> Result<u64>;
+    fn pread_u64(&mut self, pos: u64) -> Result<u64> {
         self.pread_uint(8, pos)
     }
 }
 
 // Wrapper to force endianness.
-pub struct Io<I, E> where I: Pread, E: ByteOrder {
+pub struct Io<I, E>
+    where E: ByteOrder
+{
     io: I,
     endian: PhantomData<E>,
 }
 
-impl<I, E> Io<I, E> where I: Pread, E: ByteOrder {
+impl<I, E> Io<I, E>
+    where E: ByteOrder
+{
     pub fn new(io: I) -> Self {
-        Io { io: io, endian: PhantomData }
+        Io {
+            io: io,
+            endian: PhantomData,
+        }
     }
 }
 
-impl<I, E> PreadInt for Io<I, E> where I: Pread, E: ByteOrder {
-    fn pread_uint(&mut self, nbytes: usize, pos: u64) -> std::io::Result<u64> {
+impl<I, E> PreadInt for Io<I, E>
+    where I: Pread,
+          E: ByteOrder
+{
+    fn pread_uint(&mut self, nbytes: usize, pos: u64) -> Result<u64> {
         let mut buf = vec![0; nbytes];
         try!(self.io.pread_exact(&mut buf, pos));
         Ok(E::read_uint(&buf, nbytes))
+    }
+}
+
+impl<I, E> ReadInt for Io<I, E>
+    where I: Read,
+          E: ByteOrder
+{
+    fn read_uint(&mut self, nbytes: usize) -> Result<u64> {
+        self.io.read_uint::<E>(nbytes)
     }
 }
