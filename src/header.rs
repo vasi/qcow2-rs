7ceb2a49d34f7f65eb9e193f6322a1e52c2ae488
Dave Vasilevsky <dave@vasilevsky.ca>
2016-07-08 22:25:46 -0400

    we still don't support backing files

--- src/header.rs
+++ src/header.rs
@@ -141,12 +141,12 @@ impl Header {
         }
         if self.c.backing_file_offset != 0 {
             return Err(Error::UnsupportedFeature("backing file".to_owned()));
-            if self.c.backing_file_offset > self.cluster_size() {
-                return Err(Error::FileFormat("backing file name not in first cluster".to_owned()));
-            }
-            if self.c.backing_file_size > 1023 {
-                return Err(Error::FileFormat("backing file name size too big".to_owned()));
-            }
+            // if self.c.backing_file_offset > self.cluster_size() {
+            //     return Err(Error::FileFormat("backing file name not in first cluster".to_owned()));
+            // }
+            // if self.c.backing_file_size > 1023 {
+            //     return Err(Error::FileFormat("backing file name size too big".to_owned()));
+            // }
         }
         if self.c.cluster_bits < 9 || self.c.cluster_bits > 22 {
             return Err(Error::FileFormat(format!("bad cluster_bits {}", self.c.cluster_bits)));
